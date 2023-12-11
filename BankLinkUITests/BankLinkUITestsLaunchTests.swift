8c481279c3cce4038ef80b6161babd6e0c9fd72a
Diego <Diegocortes079@gmail.com>
2023-12-10 18:47:29 -0600

    Initial Commit

---
+++ BankLinkUITests/BankLinkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BankLinkUITestsLaunchTests.swift
+//  BankLinkUITests
+//
+//  Created by Diego Cortes on 10/12/23.
+//
+
+import XCTest
+
+final class BankLinkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
