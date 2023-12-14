772d070403733fc9326a6caf20c94408d6f0cc99
Diego <Diegocortes079@gmail.com>
2023-12-13 22:58:21 -0600

    Add generic cell and items to reuse in all the views

--- BankLink/Item.swift
+++ BankLink/Item.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class Item {
+    var description: String
+    var amount: Float
+    var date: String
+    
+    init?(description: String, amount: Float, date: String) {
+        self.amount = amount
+        self.description = description
+        self.date = date
+    }
+}
--- BankLink/ItemTableViewCell.swift
+++ BankLink/ItemTableViewCell.swift
@@ -1,23 +1,73 @@
-//
-//  ItemTableViewCell.swift
-//  BankLink
-//
-//  Created by Diego Cortes on 12/12/23.
-//
-
 import UIKit
 
 class ItemTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    var AmountLabel: UILabel!
+    var DetailLabel: UILabel!
+    var ExtraLabel: UILabel?
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
         // Initialization code
+        setupUI()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func setupUI() {
+        // Create DetailLabel
+        DetailLabel = UILabel()
+        DetailLabel.textColor = .black
+        DetailLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(DetailLabel)
 
-        // Configure the view for the selected state
+        // Create AmountLabel
+        AmountLabel = UILabel()
+        AmountLabel.textColor = .black
+        AmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(AmountLabel)
+
+        // Create ExtraLabel (optional)
+        ExtraLabel = UILabel()
+        ExtraLabel?.textColor = .black
+        ExtraLabel?.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(ExtraLabel!)
+
+        // You may need to adjust the layout constraints based on your design
+        setupConstraints()
     }
 
+    private func setupConstraints() {
+        // Constraints for DetailLabel (on the left)
+        NSLayoutConstraint.activate([
+            DetailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            DetailLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+
+        // Constraints for AmountLabel (in the center)
+        NSLayoutConstraint.activate([
+            AmountLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            AmountLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+
+        // Constraints for ExtraLabel (on the right)
+        NSLayoutConstraint.activate([
+            ExtraLabel!.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            ExtraLabel!.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+    }
+
+    // Method to configure the cell with or without ExtraLabel
+    func configure(withExtraLabel: Bool) {
+        ExtraLabel?.isHidden = !withExtraLabel
+    }
+    
+    // ... (other methods)
 }
