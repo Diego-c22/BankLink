7136197ef4f4b119cced25af48cb5cfe9ab5a5fc
Diego <Diegocortes079@gmail.com>
2023-12-13 22:57:18 -0600

    Add ControllerView for credits

--- BankLink/CreditsViewController.swift
+++ BankLink/CreditsViewController.swift
@@ -1,29 +1,101 @@
 //
-//  CreditsViewController.swift
+//  InquiresView.swift
 //  BankLink
 //
-//  Created by Diego Cortes on 13/12/23.
+//  Created by Diego Cortes on 12/12/23.
 //
 
 import UIKit
 
-class CreditsViewController: UIViewController {
+class CreditsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    // @IBOutlet weak var tableView: UITableView!
     
+    var inquires = [Item]()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "ItemTableViewCell")
+        return table
+    }()
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+            view.addSubview(tableView)
+            tableView.translatesAutoresizingMaskIntoConstraints = false
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            // Set the top constraint to 250
+            let topConstraint = NSLayoutConstraint(item: tableView,
+                                                   attribute: .top,
+                                                   relatedBy: .equal,
+                                                   toItem: view.safeAreaLayoutGuide,
+                                                   attribute: .top,
+                                                   multiplier: 1,
+                                                   constant: 100)
+
+            // Activate the constraint
+            topConstraint.isActive = true
+
+            // Set up other constraints
+            NSLayoutConstraint.activate([
+                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            ])
+
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(ItemTableViewCell.self, forCellReuseIdentifier: "ItemTableViewCell")
+
+            loadSampleItems()
+        }
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    */
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return inquires.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cellIdentifier = "ItemTableViewCell"
+
+            // Create an instance of the cell class
+            let cell = ItemTableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+
+            let item = inquires[indexPath.row]
+
+            // Configure the cell
+            cell.DetailLabel?.text = item.description
+            cell.ExtraLabel?.text = String(item.amount)
+
+            return cell
+        }
+    
+    
+    func loadSampleItems(){
+        print("loading items")
+        
+        guard let item1 = Item(description: "Mortage", amount: 1000, date: "20-12-23")
+        else {
+            fatalError("Unable to instantiate car1")
+        }
+        
+        guard let item2 = Item(description: "Payroll", amount: 12000, date: "20-12-23")
+        else {
+            fatalError("Unable to instantiate car1")
+        }
+
+        
+        inquires += [item1, item2]
+        
+        
+    }
 
 }
