3673e4b46d557ccffc0ae53605b5d5838010af6f
Diego <Diegocortes079@gmail.com>
2023-12-13 22:56:43 -0600

    Add All the screens

---
+++ BankLink/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  BankLink
+//
+//  Created by Diego Cortes on 12/12/23.
+//
+
+import Foundation
---
+++ BankLink/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  BankLink
+//
+//  Created by Diego Cortes on 12/12/23.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
