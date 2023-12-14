9bc85c66b2b88f08fe0517b757c665c364dea769
Diego <Diegocortes079@gmail.com>
2023-12-13 22:58:40 -0600

    Add ControllerView for maps

--- BankLink/MapViewController.swift
+++ BankLink/MapViewController.swift
@@ -1,19 +1,55 @@
 //
 //  MapViewController.swift
-//  BankLink
+//  tablesStoryboards
 //
-//  Created by Diego Cortes on 13/12/23.
+//  Created by Diego Cortes on 25/11/23.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController /*MKMapViewDelegate*/ {
+    let map = MKMapView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(map)
+        map.frame = view.bounds
+        map.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 20.75, longitude: -103.45), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)), animated: true)
+//        map.delegate = self
+        addCustomPin(coordinates: CLLocationCoordinate2D(latitude: 20.72, longitude: -103.404))
+        addCustomPin(coordinates: CLLocationCoordinate2D(latitude: 20.74, longitude: -103.44))
+        addCustomPin(coordinates: CLLocationCoordinate2D(latitude: 20.75, longitude: -103.45))
+        addCustomPin(coordinates: CLLocationCoordinate2D(latitude: 20.72, longitude: -103.46))
+        addCustomPin(coordinates: CLLocationCoordinate2D(latitude: 20.77, longitude: -103.47))
         // Do any additional setup after loading the view.
     }
+    
+    private func addCustomPin(coordinates: CLLocationCoordinate2D) {
+        let pin = MKPointAnnotation()
+        pin.title = "BankLink Branch"
+        pin.subtitle = "Visit us"
+        pin.coordinate = coordinates
+        map.addAnnotation(pin)
+    }
+    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        guard !(annotation is MKUserLocation) else {
+//            return nil
+//        }
+//
+//        var annotationView = map.dequeueReusableAnnotationView(withIdentifier: "custom")
+//        if annotationView == nil {
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+//            annotationView?.canShowCallout = true
+//
+//        } else {
+//            annotationView?.annotation = annotation
+//        }
+//        annotationView?.image = UIImage(systemName: "location.fill")
+//        return annotationView
+//    }
     
 
     /*
